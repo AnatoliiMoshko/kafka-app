d7ae8018aabc56cfca34a9e04d9b899861d5f723
Anatolii Moshko <anatoliimoshko@gmail.com>
2024-03-27 22:36:08 +0300

    created service

--- src/main/java/com/example/kafkaapp/service/KafkaMessageService.java
+++ src/main/java/com/example/kafkaapp/service/KafkaMessageService.java
@@ -1,2 +1,21 @@
-package com.example.kafkaapp.service;public class KafkaMessageService {
+package com.example.kafkaapp.service;
+
+import com.example.kafkaapp.model.KafkaMessage;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class KafkaMessageService {
+    private final List<KafkaMessage> messages = new ArrayList<>();
+
+    public void add(KafkaMessage message) {
+        messages.add(message);
+    }
+
+    public Optional<KafkaMessage> getById(Long id) {
+        return messages.stream().filter(it -> it.getId().equals(id)).findFirst();
+    }
 }
